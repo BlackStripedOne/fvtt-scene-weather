864e56ea2f79d0a9d6d932c8a20e2f859e16cc1c
BlackStripedOne <123573329+BlackStripedOne@users.noreply.github.com>
2023-02-13 14:44:06 +0000

    Added additional constants for meteo calculations

--- scripts/constants.js
+++ scripts/constants.js
@@ -12,5 +12,7 @@ export const METEO = {
   'isaSeaLevelPa': 101325, // default air pressure for the ISA at mean sea level in Pa
   'adiabaticHyDryCoeff': -0.0065, // coefficient between dry adiabadic and hygro adiabatic cooling on ascent per meter altitude
   'g': 9.80665,
-
+  'mAir': 0.0289644, // molar mass of Earth's air: 0.0289644 kg/mol
+  'R': 8.3144598, // universal gas constant: 8.3144598 J/(mol·K)'
+  'Tzero': 237.7 // saturation vapor pressure in Kelvin, over a flat surface of water
 }
